5719146b155866d1f849441130b2acbe077ef2d9
Ken McLoughlin <mcloughlink39@gmail.com>
2017-07-01 16:39:28 +0100

    test comment modified

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -15,7 +15,7 @@ import java.util.Map;
 public class Main {
     private static StockList stockList = new StockList();
     
-    //testing remote repository
+    //testing remote repository a second time
 
     public static void main(String[] args) {
 	    StockItem temp = new StockItem("bread", 0.86, 100);
